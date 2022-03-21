562608214b97b2a6465dcbe1ad8180ddd2d1eef0
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-03-21 17:38:19 -0300

    tópico básico

--- src/Components/Titulo/index.jsx
+++ src/Components/Titulo/index.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styled from 'styled-components'
 
 const Titulo = styled.h1 `
@@ -6,7 +5,5 @@ color: grey;
 padding: 25px 0;
 `;
 
-//const Titulo = ({ children }) => {
- // return <h1 className="titulo">{children}</h1>;
-//};
+
 export default Titulo;
