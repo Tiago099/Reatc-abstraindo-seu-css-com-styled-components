4e5d00374e265365b94301d18d3db43cf888ba2b
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-03-28 19:11:19 -0300

    Projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
+import { ThemeProvider } from "styled-components";
+import { temaClaro, temaEscuro} from "./Components/UI/temas";
 
 import Cabecalho from "./Components/Cabecalho";
 import Wraper from "./Components/Container";
 import { GlobalStyle}  from './Components/GlobalStyle';
+import { BtnTema } from "./Components/UI";
+import SwitcherTema from "./Components/SwitcherTema";
 
 
 function App() {
+  const [tema, setTema] = useState(true);
+
+  const toggleTema = () => {
+    setTema((tema) => ! tema);
+  };
+
   return (
-    <>
+     <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
       <GlobalStyle/>
+      <BtnTema onClick={toggleTema}>
+      <SwitcherTema tema = {tema} />
+      </BtnTema>
       <Cabecalho />
       <Wraper/>
-    </>
+    </ThemeProvider>
+  
   );
 }
 
